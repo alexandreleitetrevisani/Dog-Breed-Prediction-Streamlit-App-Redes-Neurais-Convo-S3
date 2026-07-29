89b64a46adbc7e2d9fca95569baa4106f51e14ca
alextrevis <leitetrevisani@gmail.com>
2026-07-29 02:08:13 -0300

    fix: corrige blocos e indentação do main.tf do terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,8 @@
+# 1. Cria o Novo Repositório de Imagens Docker (Amazon ECR)
 resource "aws_ecr_repository" "app_new" {
-  name                 = var.app_name # Usará "streamlit-dog-app"
+  name                 = var.app_name
   image_tag_mutability = "MUTABLE"
 }
-
-
 
 # 2. Permissão para o App Runner puxar imagens do ECR
 resource "aws_iam_role" "apprunner_ecr_role" {
@@ -15,21 +14,18 @@ resource "aws_iam_role" "apprunner_ecr_role" {
       Action    = "sts:AssumeRole"
       Effect    = "Allow"
       Principal = { 
-        Service = "build.apprunner.amazonaws.com" 
+        Service = "://amazonaws.com" 
       }
     }]
   })
 }
-
-
 
 resource "aws_iam_role_policy_attachment" "apprunner_ecr_policy" {
   role       = aws_iam_role.apprunner_ecr_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
-
-# 3. O Serviço App Runner
+# 3. O Serviço AWS App Runner
 resource "aws_apprunner_service" "streamlit" {
   service_name = var.app_name
 
@@ -37,8 +33,8 @@ resource "aws_apprunner_service" "streamlit" {
     authentication_configuration {
       access_role_arn = aws_iam_role.apprunner_ecr_role.arn
     }
+
     image_repository {
-      # Mudado de aws_ecr_repository.app.repository_url para aws_ecr_repository.app_new.repository_url
       image_identifier      = "${aws_ecr_repository.app_new.repository_url}:${var.image_tag}"
       image_repository_type = "ECR"
       image_configuration {
@@ -47,20 +43,16 @@ resource "aws_apprunner_service" "streamlit" {
       }
     }
 
-  }
-    auto_deployments_enabled = false
- 
-
-  # REMOVEMOS O BLOCO DE HEALTH CHECK DAQUI
+    auto_deployments_enabled = false # <-- POSIÇÃO CORRETA: Fica ANTES de fechar o source_configuration
+  } # <-- Linha 51: Aqui sim fecha o source_configuration
 
   instance_configuration {
-    cpu    = "2048" # Mantemos os 4GB de RAM para o Keras rodar folgado
-    memory = "4096" 
+    cpu    = "2048" # 2 vCPU
+    memory = "4096" # 4 GB RAM para suportar o modelo Keras de 707 MB
   }
 }
 
-
-
+# 4. Output para exibir o link final no painel do GitHub Actions
 output "app_runner_url" {
   value = aws_apprunner_service.streamlit.service_url
 }
